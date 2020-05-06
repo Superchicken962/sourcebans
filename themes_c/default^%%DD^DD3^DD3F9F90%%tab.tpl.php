779ce7c0b71661c32c7d2ff02a59c590a64a5d9c
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2020-05-06 16:45:13 +0930

    hi

---
+++ themes_c/default^%%DD^DD3^DD3F9F90%%tab.tpl.php
@@ -0,0 +1,12 @@
+<?php /* Smarty version 2.6.31, created on 2020-05-06 09:13:56
+         compiled from tab.tpl */ ?>
+<?php if ($this->_tpl_vars['active']): ?>
+	<li class="active">
+<?php else: ?>
+	<li class="nonactive">
+<?php endif; ?>
+		<?php echo $this->_tpl_vars['tab_link']; ?>
+
+</li>
+
+	
